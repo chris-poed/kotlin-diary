8ea5e403447db26db8019b512b1f3e620395acc5
chris-poed <christianpoed@gmail.com>
2026-05-08 11:10:11 +0100

    added http4k

--- projects/diary/src/main/kotlin/Main.kt
+++ projects/diary/src/main/kotlin/Main.kt
@@ -1,4 +1,9 @@
 package com.makers.diary
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.server.Undertow
+import org.http4k.server.asServer
 
 import java.util.*
 
@@ -15,5 +20,12 @@ fun main() {
     val reminder = Reminder("Chris")
     reminder.remindMeTo("Fit the hose")
     println(reminder.remind())
+
+    val app = { request: Request ->
+        Response(Status.OK)
+            .body("Hello, ${request.query("name")}!")
+    }
+
+    app.asServer(Undertow(9000)).start()
 
 }
